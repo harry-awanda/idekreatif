72e408c9d45b7bda16ef0d98e166996dbf73019e
harry-awanda <harrywanda0704@gmail.com>
2024-11-29 22:45:02 +0700

    minor update on header

--- .includes/header.php
+++ .includes/header.php
@@ -21,19 +21,11 @@ unset($_SESSION['notification']); // Hapus notifikasi setelah ditampilkan
 ?>
 
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style layout-menu-fixed"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="assets/"
-  data-template="vertical-menu-template-free"
->
+<html lang="en">
   <head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    <meta name="viewport" content="width=device-width,
+    initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
     <title>Dashboard &mdash; IdeKreatif</title>
