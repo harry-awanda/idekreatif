b85c7a3c220e06c4c1207b06fdb185c4564cd064
harry-awanda <harrywanda0704@gmail.com>
2024-11-29 22:41:09 +0700

    update bootstrap asset

--- .includes/footer.php
+++ .includes/footer.php
@@ -29,8 +29,7 @@
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     <script src="assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="assets/vendor/libs/popper/popper.js"></script>
-    <script src="assets/vendor/js/bootstrap.js"></script>
+    <script src="assets/vendor/js/bootstrap.bundle.min.js"></script>
     <script src="assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
     <script src="assets/vendor/js/menu.js"></script>
     <!-- endbuild -->
