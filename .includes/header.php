5cc1cd4f7e851cdfc04f94de086c82e6f0d245cf
harry-awanda <harrywanda0704@gmail.com>
2024-11-30 09:54:04 +0700

    Refactor session handling logic for improved readability and reliability

--- .includes/header.php
+++ .includes/header.php
@@ -1,23 +1,29 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["username"]) || !isset($_SESSION["role"])) {
-    header('location: ./auth/login.php');
+// Periksa apakah sesi username dan role sudah ada, jika tidak arahkan ke halaman login
+if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
+  header('Location: ./auth/login.php');
+  exit(); // Pastikan script berhenti setelah pengalihan
 }
-if (isset($_SESSION["user_id"])) {
-  $userId = $_SESSION["user_id"];
-} else {
-  // Handle the case where user_id is not set in the session
+
+// Pastikan user_id ada dalam sesi, jika tidak tampilkan pesan kesalahan dan hentikan eksekusi
+if (empty($_SESSION["user_id"])) {
   echo "Error: user_id not set in the session.";
   exit();
 }
+
+// Ambil data dari sesi
+$userId = $_SESSION["user_id"];
 $username = $_SESSION["username"];
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 
-$notification = isset($_SESSION['notification']) ? $_SESSION['notification'] : null;
-unset($_SESSION['notification']); // Hapus notifikasi setelah ditampilkan
-
+// Ambil notifikasi jika ada, kemudian hapus dari sesi
+$notification = $_SESSION['notification'] ?? null;
+if ($notification) {
+  unset($_SESSION['notification']);
+}
 ?>
 
 <!DOCTYPE html>
