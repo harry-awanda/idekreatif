60fe9043f5f0c151873e0ce86a7c4a7e832f9a25
harry-awanda <harrywanda0704@gmail.com>
2024-11-30 13:39:21 +0700

    fixed blog content

--- blog/content.php
+++ blog/content.php
@@ -1,5 +1,5 @@
 <?php 
-include 'config.php';
+include '../config.php';
 include 'header.php';
 
 // Pastikan id_post tersedia dan valid
@@ -35,7 +35,7 @@ if (isset($_GET['id_post']) && is_numeric($_GET['id_post'])) {
       <div class="col-lg-8 entries">
         <article class="entry entry-single">
           <div class="entry-img">
-            <img src="<?= $post['image_path']; ?>" alt="" class="img-fluid">
+            <img src="../<?= $post['image_path']; ?>" alt="" class="img-fluid">
           </div>
           <h2 class="entry-title">
             <a href="#"><?= $post['post_title']; ?></a>
@@ -57,39 +57,7 @@ if (isset($_GET['id_post']) && is_numeric($_GET['id_post'])) {
           </div>
         </article><!-- End blog entry -->
       </div><!-- End blog entries list -->
-      <div class="col-lg-4">
-        <div class="sidebar">
-          <h3 class="sidebar-title">Categories</h3>
-          <div class="sidebar-item categories">
-            <ul>
-              <?php
-                // Ambil data kategori dari tabel categories
-                $categoryQuery = "SELECT * FROM categories";
-                $categoryResult = mysqli_query($conn, $categoryQuery);
-                while ($category = mysqli_fetch_assoc($categoryResult)) :
-              ?>
-              <li><a href="#"><?= $category['category_name']; ?></a></li>
-              <?php endwhile; ?>
-            </ul>
-          </div><!-- End sidebar categories-->
-          <h3 class="sidebar-title">Recent Posts</h3>
-          <div class="sidebar-item recent-posts">
-            <?php
-              // Query untuk mendapatkan postingan terbaru
-              $recentPostsQuery = "SELECT post_title, created_at FROM posts ORDER BY created_at DESC LIMIT 5";
-              $recentPostsResult = mysqli_query($conn, $recentPostsQuery);
-              while ($recentPost = mysqli_fetch_assoc($recentPostsResult)) :
-            ?>
-            <div class="post-item clearfix">
-              <!-- Gambar post (gantilah dengan path yang sesuai jika ada) -->
-              <img src="assets/blog/img/blog/blog-recent-1.jpg" alt="">
-              <h4><a href="blog-single.html"><?= $recentPost['post_title']; ?></a></h4>
-              <time ><?= date('M j, Y', strtotime($recentPost['created_at'])); ?></time>
-            </div>
-            <?php endwhile; ?>
-          </div><!-- End sidebar recent posts-->
-        </div><!-- End sidebar recent posts-->
-      </div><!-- End sidebar -->
+      <?php include "sidebar.php"; ?>
     </div><!-- End blog sidebar -->
   </div>
 </section><!-- End Blog Section -->
--- blog/index.php
+++ blog/index.php
@@ -72,39 +72,7 @@ include 'header.php';
           </ul>
         </div>
       </div><!-- End blog entries list -->
-      <div class="col-lg-4">
-        <div class="sidebar">
-          <h3 class="sidebar-title">Categories</h3>
-          <div class="sidebar-item categories">
-            <ul>
-              <?php
-                // Ambil data kategori dari tabel categories
-                $categoryQuery = "SELECT * FROM categories";
-                $categoryResult = mysqli_query($conn, $categoryQuery);
-                while ($category = mysqli_fetch_assoc($categoryResult)) :
-              ?>
-              <li><a href="#"><?= $category['category_name']; ?></a></li>
-              <?php endwhile; ?>
-            </ul>
-          </div><!-- End sidebar categories-->
-          <h3 class="sidebar-title">Recent Posts</h3>
-          <div class="sidebar-item recent-posts">
-            <?php
-              // Query untuk mendapatkan postingan terbaru
-              $recentPostsQuery = "SELECT post_title, created_at, image_path FROM posts ORDER BY created_at DESC LIMIT 5";
-              $recentPostsResult = mysqli_query($conn, $recentPostsQuery);
-              while ($recentPost = mysqli_fetch_assoc($recentPostsResult)) :
-            ?>
-            <div class="post-item clearfix">
-              <!-- Gambar post (gantilah dengan path yang sesuai jika ada) -->
-              <img src="../<?= $recentPost['image_path']; ?>" alt="">
-              <h4><a href="#"><?= $recentPost['post_title']; ?></a></h4>
-              <time><?= date('M j, Y', strtotime($recentPost['created_at'])); ?></time>
-            </div>
-            <?php endwhile; ?>
-          </div><!-- End sidebar recent posts-->
-        </div><!-- End sidebar recent posts-->
-      </div><!-- End sidebar -->
+      <?php include "sidebar.php"; ?>
     </div><!-- End blog sidebar -->
   </div>
 </section><!-- End Blog Section -->
